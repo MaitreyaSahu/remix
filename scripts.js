84f1c72bd6698a37acf67a896246fad0ffcdcaca
Maitreya <mtsahu1995@gmail.com>
2021-04-10 02:42:26 +0530

    added touch

--- scripts.js
+++ scripts.js
@@ -3,9 +3,10 @@
 document.addEventListener('keydown', play);
 
 function play(e){
-    //console.log(e.key, e.keyCode);
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+    //console.log(e.target.dataset.key);
+    const keyCode = e.target.dataset.key || e.keyCode;
+    const audio = document.querySelector(`audio[data-key = "${keyCode}"]`);
+    const key = document.querySelector(`div[data-key = "${keyCode}"]`);
 
     //console.log(audio);
     //console.log(key);
@@ -19,7 +20,10 @@ function play(e){
 
 const keys = document.querySelectorAll('.key');
 //console.log(keys);
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', play)
+});
 
 function removeTransition(e){
     //console.log(e)
